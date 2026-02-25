588d695fad1ce05311ac256c35d072b2ad2eda08
Triggerossi <makardemedec@gmail.com>
2026-02-25 10:49:09 +0300

    dodelati

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"time"
 )
@@ -11,8 +12,19 @@ func main() {
 
 	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`{"Status": "ok"}`))
-		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("application/json", "Content-Type")
+		http.MaxBytesReader()
 	})
 	server := &http.Server{Addr: ":8080", Handler: mux, ReadHeaderTimeout: 5 * time.Second}
 	server.ListenAndServe()
+}
+
+const maxBodyBytes = 1024 * 1024
+
+func readJson(w http.ResponseWriter, r *http.Request, output any) error {
+	r.Body = http.MaxBytesReader(w, r.Body, maxBodyBytes)
+
+	dec := json.NewDecoder(r.Body)
+	if err := dec.Decode(output); err != nil {
+	}
 }
